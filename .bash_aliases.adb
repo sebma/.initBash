bbf79b64b85b1110d797efbf89e82b8a2f4f2769
sebma <sebma@users.noreply.github.com>
2019-12-10 14:42:29 +0100

    Updated .bash_aliases.adb

--- .bash_aliases.adb
+++ .bash_aliases.adb
@@ -5,6 +5,7 @@ test "$debug" -gt 0 && echo "=> Running $bold${colors[blue]}$(basename ${BASH_SO
 which adb >/dev/null 2>&1 && {
 export adb=$(which adb)
 export dos2unix="$(which tr) -d '\r'"
+alias adb_getprop="$adb shell getprop"
 alias adbFindPackages="$adb shell pm list packages -f -i"
 alias adbGetAndroidVersion="$adb shell getprop ro.build.version.release | $dos2unix"
 alias adbGetArch="$adb shell getprop ro.product.cpu.abi | $dos2unix"
@@ -13,8 +14,13 @@ alias adbGetBrand="$adb shell getprop ro.product.brand | $dos2unix"
 alias adbGetCodeName="$adb shell getprop ro.product.device | $dos2unix"
 alias adbGetManufacturer="$adb shell getprop ro.product.manufacturer | $dos2unix"
 alias adbGetModel="$adb shell getprop ro.product.model | $dos2unix"
-alias adbGetNetworkIP="$adb shell ip -o addr | egrep -v '(127.0.0.|169.254.0|inet6|loopback)' | awk -F ' *|/' '/inet /{print\$4}' | $dos2unix"
-alias adbGetNetworkInterface="$adb shell ip -o addr | egrep -v '(127.0.0.|169.254.0|inet6|loopback)' | awk '/inet /{print\$2}' | $dos2unix"
+
+typeset androidDeviceNetworkInterface=$($adb shell getprop wifi.interface | $dos2unix)
+if [ -n "$androidDeviceNetworkInterface" ];then
+	alias adbGetNetworkInterface="echo $androidDeviceNetworkInterface"
+	alias adbGetNetworkIP="$adb shell ip -o addr show $androidDeviceNetworkInterface | awk -F ' *|/' '/inet /{print\$4}' | $dos2unix"
+fi
+
 alias adbGetSDK="$adb shell getprop ro.build.version.sdk | $dos2unix"
 alias adbGetSerial="$adb shell getprop ro.serialno | $dos2unix"
 }
