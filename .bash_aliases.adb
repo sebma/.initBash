7034733ab6bafa99dc9a4727d9414e0036d47b67
sebma <sebma@users.noreply.github.com>
2019-12-13 12:54:54 +0100

    Updated .bash_aliases.adb

--- .bash_aliases.adb
+++ .bash_aliases.adb
@@ -4,24 +4,30 @@ test "$debug" -gt 0 && echo "=> Running $bold${colors[blue]}$(basename ${BASH_SO
 
 export adb=$(which adb)
 export dos2unix="$(which tr) -d '\r'"
-alias adb_getprop="$adb shell getprop"
-alias adbFindPackages="$adb shell pm list packages"
-alias adbGetADBTcpPort="$adb shell getprop service.adb.tcp.port"
+alias adb_getprop="$adb shell getpro | $dos2unix"
+alias adbCpuInfo="$adb shell cat /proc/cpuinf | $dos2unix"
+alias adbMemInfo="$adb shell cat /proc/meminf | $dos2unix"
+alias adbBuildInfo="$adb shell cat /system/build.pro | $dos2unix"
+alias adbFindPackages="$adb shell pm list package"
+alias adbGetADBTcpPort="$adb shell getprop service.adb.tcp.por | $dos2unix"
 alias adbGetAndroidVersion="$adb shell getprop ro.build.version.release | $dos2unix"
 alias adbGetArch="$adb shell getprop ro.product.cpu.abi | $dos2unix"
 alias adbGetBatteryLevel="$adb shell dumpsys battery | grep level | $dos2unix"
 alias adbGetBrand="$adb shell getprop ro.product.brand | $dos2unix"
 alias adbGetCodeName="$adb shell getprop ro.product.device | $dos2unix"
-alias adbGetExtSDCardMountPoint="$adb shell mount | awk '/emulated|sdcard0/{next}/(Removable|storage)\//{if(\$2==\"on\")print\$3;else print\$2}'"
+alias adbGetExtSDCardMountPoint="$adb shell mount | awk '/emulated|sdcard0/{next}/(Removable|storage)\//{if(\$2==\"on\")print\$3;else print\$2} | $dos2unix"
+alias adbGetIMEI="{ $adb shell getprop persist.radio.device.imei | \grep [0-9] || $adb shell dumpsys iphonesubinfo | awk '/Device/{print\$NF}' | \grep [0-9] || $adb shell getprop | awk '/persist.sys.fota_deviceid.*35/{print\$NF}'; } | $dos2unix"
 alias adbGetManufacturer="$adb shell getprop ro.product.manufacturer | $dos2unix"
 alias adbGetModel="$adb shell getprop ro.product.model | $dos2unix"
 
-alias adbGetNetworkInterface="$adb shell getprop wifi.interface | $dos2unix"
-$adb shell echo >/dev/null 2>&1 && export androidDeviceNetworkInterface=$(adbGetNetworkInterface)
-if [ -n "$androidDeviceNetworkInterface" ];then
-	alias adbGetNetworkIP="{ $adb shell getprop dhcp.${androidDeviceNetworkInterface/:*/}.ipaddress | grep [0-9] || $adb shell ip -o addr show $androidDeviceNetworkInterface | awk -F ' *|/' '/inet /{print\$4}'; } | $dos2unix"
+alias adbGetWlanInterface="$adb shell getprop wifi.interface | $dos2unix"
+$adb shell echo >/dev/null 2>&1 && export androidWlanInterface=$(adbGetWlanInterface)
+if [ -n "$androidWlanInterface" ];then
+	alias adbGetWlanIP="{ $adb shell getprop dhcp.${androidWlanInterface/:*/}.ipaddress | \grep [0-9] || $adb shell ip -o addr show $androidWlanInterface | awk -F ' *|/' '/inet /{print\$4}'; } | $dos2unix"
+	alias adbGetWlanMAC="$adb shell cat /sys/class/net/${androidWlanInterface/:*/}/addres | $dos2unix"
 fi
 
+alias adbGetProp="$adb shell getpro | $dos2unix"
 alias adbGetSDK="$adb shell getprop ro.build.version.sdk | $dos2unix"
 alias adbGetSerial="$adb shell getprop ro.serialno | $dos2unix"
 function adbSetADBTcpPort {
@@ -39,8 +45,8 @@ function adbSetADBTcpPort {
 	$adb devices | grep -w offline && echo "[ $FUNCNAME ] => INFO : You need to unplug your USB cord." >&2 && return 3
 	$adb shell echo >/dev/null || return
 
-	local androidDeviceNetworkInterface=$($adb shell getprop wifi.interface | $dos2unix)
-	local adbGetNetworkIP=$($adb shell ip -o addr show $androidDeviceNetworkInterface | awk -F ' *|/' '/inet /{print$4}' | $dos2unix)
+	local androidWlanInterface=$($adb shell getprop wifi.interface | $dos2unix)
+	local adbGetWlanIP=$($adb shell ip -o addr show $androidWlanInterface | awk -F ' *|/' '/inet /{print$4}' | $dos2unix)
 
 	local currentADBTcpPortNum=$($adb shell getprop service.adb.tcp.port)
 	if echo $currentADBTcpPortNum | \egrep -q "[0-9]+";then
@@ -52,7 +58,7 @@ function adbSetADBTcpPort {
 	set +x
 	$adb tcpip $port
 	sleep 1
-	$adb connect $adbGetNetworkIP:$port | grep -w connected && echo "[ $FUNCNAME ] => INFO : You can now unplugg the USB cord and run your <adb shell> commands." >&2
+	$adb connect $adbGetWlanIP:$port | grep -w connected && echo "[ $FUNCNAME ] => INFO : You can now unplugg the USB cord and run your <adb shell> commands." >&2
 	sleep 2
 	if $adb devices | grep -w offline || $adb shell echo 2>&1 | grep 'more than one';then
 		echo "[ $FUNCNAME ] => INFO : You need to unplug your USB cord." >&2
