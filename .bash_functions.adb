a2187c42412b32f342aa3f381131ab7a4d03d0dd
sebma <sebma@users.noreply.github.com>
2020-03-10 16:17:59 +0100

    Updated .bash_functions.adb

---
+++ .bash_functions.adb
@@ -0,0 +1,38 @@
+# vim: set ft=bash noet:
+declare -A | grep -wq colors || source $initDir/.colors
+test "$debug" -gt 0 && echo "=> Running $bold${colors[blue]}$(basename ${BASH_SOURCE[0]})$normal ..."
+
+export adb=$(which adb)
+export dos2unix="$(which tr) -d '\r'"
+
+function adbGetAndroidCodeName {
+	local androidRelease=$($adb shell getprop ro.build.version.release | $dos2unix | cut -d. -f1-2 | tr -d .)
+
+	local androidCodeName=$($adb shell getprop ro.build.version.codename | $dos2unix)
+	test $androidCodeName = REL && {
+		androidCodeName="${colors[blue]}"
+		case $androidRelease in
+		10) androidCodeName+=NoCodename;;
+		11) androidCodeName+="Petit Four";;
+		15) androidCodeName+=Cupcake;;
+		20|21) androidCodeName+=Eclair;;
+		22) androidCodeName+=FroYo;;
+		23) androidCodeName+=Gingerbread;;
+		30|31|32) androidCodeName+=Honeycomb;;
+		40) androidCodeName+="Ice Cream Sandwich";;
+		41|42|43) androidCodeName+="Jelly Bean";;
+		44) androidCodeName+=KitKat;;
+		50|51) androidCodeName+=Lollipop;;
+		60) androidCodeName+=Marshmallow;;
+		70|71) androidCodeName+=Nougat;;
+		80|81) androidCodeName+=Oreo;;
+		90) androidCodeName+=Pie;;
+		100) androidCodeName+=ToBeReleased;;
+		*) androidCodeName=${colors[red]}unknown;;
+		esac
+	}
+	echo $androidCodeName$normal
+}
+
+set +x
+test "$debug" -gt 0 && echo "=> END of $bold${colors[blue]}$(basename ${BASH_SOURCE[0]})$normal"
