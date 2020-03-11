39fb397ab24983ad7829a4cdb590fb9f203879ab
sebma <sebma@users.noreply.github.com>
2020-03-11 11:56:02 +0100

    Updated .bash_aliases.adb

--- .bash_aliases.adb
+++ .bash_aliases.adb
@@ -4,121 +4,35 @@ test "$debug" -gt 0 && echo "=> Running $bold${colors[blue]}$(basename ${BASH_SO
 
 export adb=$(which adb)
 export dos2unix="$(which tr) -d '\r'"
-alias adb_getprop="$adb shell getprop"
-alias adb_getprop_grep="$adb shell getprop | $dos2unix | grep -P"
+export getprop="$adb shell getprop"
+alias adb_getprop="$getprop"
+alias adb_getprop_grep="$getprop | $dos2unix | grep -P"
 alias adbCpuInfo="$adb shell cat /proc/cpuinf | $dos2unix"
 alias adbMemInfo="$adb shell cat /proc/meminf | $dos2unix"
 alias adbBuildInfo="$adb shell cat /system/build.pro | $dos2unix"
 alias adbEnablePackage="$adb shell pm enable"
 alias adbFindPackages="$adb shell pm list packages"
-alias adbGetADBTcpPort="$adb shell getprop service.adb.tcp.por | $dos2unix"
-alias adbGetAndroidSDK="$adb shell getprop ro.build.version.sdk | $dos2unix"
-alias adbGetAndroidVersion="$adb shell getprop ro.build.version.release | $dos2unix"
-alias adbGetArch="$adb shell getprop ro.product.cpu.abi | $dos2unix"
+alias adbGetADBTcpPort="$getprop service.adb.tcp.por | $dos2unix"
+alias adbGetAndroidSDK="$getprop ro.build.version.sdk | $dos2unix"
+alias adbGetAndroidVersion="$getprop ro.build.version.release | $dos2unix"
+alias adbGetArch="$getprop ro.product.cpu.abi | $dos2unix"
 alias adbGetBatteryLevel="$adb shell dumpsys battery | grep level | $dos2unix"
-alias adbGetBrand="$adb shell getprop ro.product.brand | $dos2unix"
-alias adbGetDeviceCodeName="$adb shell getprop ro.product.device | $dos2unix"
+alias adbGetBrand="$getprop ro.product.brand | $dos2unix"
+alias adbGetDeviceCodeName="$getprop ro.product.device | $dos2unix"
 alias adbGetExtSDCardMountPoint="$adb shell mount | awk '/emulated|sdcard0/{next}/(Removable|storage)\//{if(\$2==\"on\")print\$3;else print\$2} | $dos2unix"
-alias adbGetManufacturer="$adb shell getprop ro.product.manufacturer | $dos2unix"
-alias adbGetModel="$adb shell getprop ro.product.model | $dos2unix"
+alias adbGetManufacturer="$getprop ro.product.manufacturer | $dos2unix"
+alias adbGetModel="$getprop ro.product.model | $dos2unix"
 
-alias adbGetWlanInterface="$adb shell getprop wifi.interface | $dos2unix"
+alias adbGetWlanInterface="$getprop wifi.interface | $dos2unix"
 $adb shell echo >/dev/null 2>&1 && export androidWlanInterface=$(adbGetWlanInterface)
 if [ -n "$androidWlanInterface" ];then
-	alias adbGetWlanIP="{ $adb shell getprop dhcp.${androidWlanInterface/:*/}.ipaddress | \grep [0-9] || $adb shell ip -o addr show $androidWlanInterface | awk -F ' *|/' '/inet /{print\$4}'; } | $dos2unix"
+	alias adbGetWlanIP="{ $getprop dhcp.${androidWlanInterface/:*/}.ipaddress | \grep [0-9] || $adb shell ip -o addr show $androidWlanInterface | awk -F ' *|/' '/inet /{print\$4}'; } | $dos2unix"
 	alias adbGetWlanMAC="$adb shell cat /sys/class/net/${androidWlanInterface/:*/}/addres | $dos2unix"
 fi
 
-alias adbGetProp="$adb shell getprop"
-alias adbGetPropGrep="$adb shell getprop | $dos2unix | grep -P"
-alias adbGetSerial="$adb shell getprop ro.serialno | $dos2unix"
-function adbDisablePackages {
-	for package
-	do
-		echo "=> Disabling <$package> #$remainingPackages/$machingPackagesNumber remaining packages to process ..."
-		set -x
-		$adb shell pm disable $package || $adb shell pm disable-until-used $package || $adb shell pm disable-user $package
-		set +x
-		let remainingPackages--
-	done | $dos2unix
-}
-function adbGetIMEI {
-	local imeiLength=15
-	local IMEI1=$(printf "%0${imeiLength}d" 0)
-	local IMEI2=$IMEI1
-
-	if IMEI1=$($adb shell getprop persist.sys.fota_deviceid1); echo $IMEI1 | \egrep -q "^[0-9]{$imeiLength}"; then
-		IMEI2=$($adb shell getprop persist.sys.fota_deviceid2 | sed 's/^,//')
-	elif IMEI1=$($adb shell service call iphonesubinfo 1 | awk -F"'" 'NR>1{gsub(/\./,"",$2);imei=imei$2}END{print imei}'); echo $IMEI1 | \egrep -q "^[0-9]{$imeiLength}"; then
-		IMEI2=$($adb shell service call iphonesubinfo 3 | awk -F"'" 'NR>1{gsub(/\./,"",$2);imei=imei$2}END{print imei}')
-	elif IMEI1=$($adb shell dumpsys iphonesubinfo | awk '/Device/{print$NF}'); echo $IMEI1 | \egrep -q "^[0-9]{$imeiLength}"; then
-		IMEI2=$($adb shell dumpsys iphonesubinfo2 2>/dev/null | awk '/Device/{print$NF}' )
-	elif IMEI1=$($adb shell getprop persist.radio.device.imei); echo $IMEI1 | \egrep -q "^[0-9]{$imeiLength}"; then
-		:
-	fi
-
-	IMEI1=$(echo $IMEI1 | $dos2unix)
-	echo $IMEI1
-	IMEI2=$(echo $IMEI2 | $dos2unix)
-	echo $IMEI2
-}
-function adbHidePackages {
-	for package
-	do
-		echo "=> Hiding <$package> #$remainingPackages/$machingPackagesNumber remaining packages to process ..."
-		$adb shell pm hide $package || $adb shell pm disable-until-used $package || $adb shell pm disable-user $package
-		let remainingPackages--
-	done | $dos2unix
-}
-function adbPackageStatus {
-	for package
-	do
-		echo "=> Checking <$package> #$remainingPackages/$machingPackagesNumber remaining packages to process ..."
-		if $adb shell pm list packages -e | \egrep -q "$package"; then
-			echo "==> $package is ENABLED"
-		else
-			echo "==> $package is NOT ENABLED"
-		fi
-		let remainingPackages--
-	done | $dos2unix
-}
-function adbSetADBTcpPort {
-	local defaultADBTcpPort=5555
-	local port=0
-	local adb=$(which adb)
-	local dos2unix="$(which tr) -d '\r'"
-	case $1 in
-		-h|-help|--h|--help) echo "=> Usage: $FUNCNAME [portNumber|$defaultADBTcpPort]" >&2; return 1 ;;
-		[0-9]) port=$1;;
-		"") port=$defaultADBTcpPort;;
-		*) echo "[ $FUNCNAME ] => ERROR Usage: $FUNCNAME [portNumber|$defaultADBTcpPort]" >&2; return 2 ;;
-	esac
-
-	$adb devices | grep -w offline && echo "[ $FUNCNAME ] => INFO : You need to unplug your USB cord." >&2 && return 3
-	$adb shell echo >/dev/null || return
-
-	local androidWlanInterface=$($adb shell getprop wifi.interface | $dos2unix)
-	local adbGetWlanIP=$($adb shell ip -o addr show $androidWlanInterface | awk -F ' *|/' '/inet /{print$4}' | $dos2unix)
-
-	local currentADBTcpPortNum=$($adb shell getprop service.adb.tcp.port)
-	if echo $currentADBTcpPortNum | \egrep -q "[0-9]+";then
-		echo "[ $FUNCNAME ] => INFO : The <service.adb.tcp.port> parameter is already set to $currentADBTcpPortNum." >&2
-		set -x;$adb tcpip $currentADBTcpPortNum;set +x
-		return 4
-	fi
-
-	set +x
-	$adb tcpip $port
-	sleep 1
-	$adb connect $adbGetWlanIP:$port | grep -w connected && echo "[ $FUNCNAME ] => INFO : You can now unplugg the USB cord and run your <adb shell> commands." >&2
-	sleep 2
-	if $adb devices | grep -w offline || $adb shell echo 2>&1 | grep 'more than one';then
-		echo "[ $FUNCNAME ] => INFO : You need to unplug your USB cord." >&2
-		return 5
-	fi
-	$adb shell echo >/dev/null || echo "[ $FUNCNAME ] => INFO : If the <adbd> service could not restart automatically, you need to re-enable <USB Debugging> on the android device manually." >&2
-	set +x
-}
+alias adbGetProp="$getprop"
+alias adbGetPropGrep="$getprop | $dos2unix | grep -P"
+alias adbGetSerial="$getprop ro.serialno | $dos2unix"
 
 set +x
 test "$debug" -gt 0 && echo "=> END of $bold${colors[blue]}$(basename ${BASH_SOURCE[0]})$normal"
